43d6b3d2b52081be10d9cbd4fffa3240a62c8938
sfox123 <shagyfox133@gmail.com>
2021-09-01 14:22:43 +0530

    loaded toolbar

--- public/app.js
+++ public/app.js
@@ -3,4 +3,6 @@ $(".tab__link").click((e) => {
   $(".choosen").removeClass("choosen");
   $(e.target).addClass("choosen");
   $(".map__stage").attr("src", `./img/Maps/${id}`);
+  $("#zoom").attr("href", `./img/Maps/${id}`);
+  $("#download").attr("href", `./img/Maps/${id}`);
 });
